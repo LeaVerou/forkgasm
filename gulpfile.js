0b0c332ada08e75830a5fe3dece490991db00513
Lea Verou <lea@verou.me>
2017-09-19 20:28:12 -0400

    Recipe index & view individual recipes!

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var rename = require("gulp-rename");
 var postcss = require('gulp-postcss');
+var fileinclude = require("gulp-file-include");
 
 gulp.task('css', function () {
 	return gulp.src(["**/*.src.css", "!node_modules/**"])
@@ -22,9 +23,21 @@ gulp.task('css', function () {
 		.pipe(gulp.dest('.'));
 });
 
+gulp.task("html", function() {
+	gulp.src(["**/*.tpl.html"])
+		.pipe(fileinclude({
+			basepath: "templates/"
+		}).on("error", function(error) {
+			console.error(error);
+		}))
+		.pipe(rename({ extname: "" }))
+		.pipe(rename({ extname: ".html" }))
+		.pipe(gulp.dest("."))
+});
 
 gulp.task("watch", function() {
 	gulp.watch(["**/*.src.css"], ["css"]);
+	gulp.watch(["**/*.tpl.html", "./templates/*.html"], ["html"]);
 });
 
 gulp.task("default", ["css"]);
