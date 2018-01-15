eb211d334573a07d5fae284a1e834e79749ed66a
Lea Verou <lea@verou.me>
2018-01-15 22:49:07 +0200

    Add thumbnails

--- forkgasm.js
+++ forkgasm.js
@@ -45,6 +45,12 @@ if ("serviceWorker" in navigator) {
 
 })();
 
-function thumbnail(instagramURL, width) {
-	return instagramURL.replace(/\/(e\d5)\//, `/s${width}x${width}/$1/`);
+function thumbnail(url) {
+	if (url.indexOf("/images/dishes/") === 0) {
+		// Local
+		return url.replace("/images/dishes/", "/images/dishes/thumbs/");
+	}
+
+	// else it's an Instagram URL
+	return url.replace(/\/(e\d5)\//, `/s240x240/$1/`);
 }
