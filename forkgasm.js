e5cd2dafa1b23e0623fde7a16c42d6e4b89834fc
Lea Verou <lea@verou.me>
2021-12-14 15:28:32 +0200

    Add Michelin distinctions

--- forkgasm.js
+++ forkgasm.js
@@ -2,7 +2,7 @@ function thumbnail(url) {
 	if (!url) {
 		return url;
 	}
-	
+
 	if (url.indexOf("/images/dishes/") === 0) {
 		// Local
 		return url.replace("/images/dishes/", "/images/dishes/thumbs/");
@@ -10,4 +10,17 @@ function thumbnail(url) {
 
 	// else it's an Instagram URL
 	return url.replace(/\/(e\d5)\//, "/s240x240/$1/");
+}
+
+
+function getAwardURL(award, restaurant) {
+	if (!award) {
+		return null;
+	}
+
+	if (award.startsWith("michelin-")) {
+		return `https://guide.michelin.com/gr/en/search?q=${restaurant.name}`;
+	}
+
+	return "";
 }
